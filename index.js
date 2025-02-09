4db3a23df9e7a72cee18a972124a67115b1dcea6
shahinalom <shahinalom3511371@gmail.com>
2025-02-09 20:16:15 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+
+// Mongobd Connect Start
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sozmemk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+      serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+      }
+});
+async function run() {
+      try {
+            await client.connect();
+
+
+
+            const menuCollection = client.db('Bistroboss').collection('menu')
+            const reviewsCollection = client.db('Bistroboss').collection('reviews')
+
+            app.get("/menu", async (req, res) => {
+                  const result = await menuCollection.find().toArray()
+                  res.send(result)
+            })
+            app.get("/reviews", async (req, res) => {
+                  const result = await reviewsCollection.find().toArray()
+                  res.send(result)
+            })
+
+
+            await client.db("admin").command({ ping: 1 });
+            console.log("Mongodb Connected successfully!");
+      } finally {
+      }
+}
+run().catch(console.dir);
+// Mongobd Connect End
+
+
+
+
+app.get('/', (req, res) => {
+      res.send('Bistro is Running!')
+})
+app.listen(port, () => {
+      console.log(`Bistro boss is Sitting On Port ${port}`)
+})
