82a62f7e3ca336d5be06ebb10ba9b0335c4ca976
shahinalom <shahinalom3511371@gmail.com>
2025-02-22 22:31:08 +0500

    data added

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
 
             const menuCollection = client.db('Bistroboss').collection('menu')
             const reviewsCollection = client.db('Bistroboss').collection('reviews')
+            const cartsCollection = client.db('Bistroboss').collection('carts')
 
             app.get("/menu", async (req, res) => {
                   const result = await menuCollection.find().toArray()
@@ -33,6 +34,17 @@ async function run() {
             })
             app.get("/reviews", async (req, res) => {
                   const result = await reviewsCollection.find().toArray()
+                  res.send(result)
+            })
+
+            // Carts Collection
+            app.get("/carts", async (req, res) => {
+                  const result = await cartsCollection.find().toArray()
+                  res.send(result)
+            })
+            app.post('/carts', async (req, res) => {
+                  const cartItem = req.body;
+                  const result = await cartsCollection.insertOne(cartItem)
                   res.send(result)
             })
 
