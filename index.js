9d7445c6c0fe8444ef0a6e37cce3dddf5f5a00e4
shahinalom <shahinalom3511371@gmail.com>
2025-02-28 22:10:53 +0600

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 
 // Mongobd Connect Start
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sozmemk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
       serverApi: {
@@ -27,6 +27,7 @@ async function run() {
             const menuCollection = client.db('Bistroboss').collection('menu')
             const reviewsCollection = client.db('Bistroboss').collection('reviews')
             const cartsCollection = client.db('Bistroboss').collection('carts')
+            const userCollection = client.db('Bistroboss').collection('users')
 
             app.get("/menu", async (req, res) => {
                   const result = await menuCollection.find().toArray()
@@ -37,9 +38,19 @@ async function run() {
                   res.send(result)
             })
 
+
+            // User Related Data
+            app.post("/users", async (req, res) => {
+                  const user = req.body;
+                  const result = await userCollection.insertOne(user)
+                  res.send(result)
+            })
+
             // Carts Collection
             app.get("/carts", async (req, res) => {
-                  const result = await cartsCollection.find().toArray()
+                  const email = req.query.email
+                  const query = { email: email }
+                  const result = await cartsCollection.find(query).toArray()
                   res.send(result)
             })
             app.post('/carts', async (req, res) => {
@@ -47,7 +58,12 @@ async function run() {
                   const result = await cartsCollection.insertOne(cartItem)
                   res.send(result)
             })
-
+            app.delete('/carts/:id', async (req, res) => {
+                  const id = req.params.id
+                  const query = { _id: new ObjectId(id) }
+                  const result = await cartsCollection.deleteOne(query)
+                  res.send(result)
+            })
 
             await client.db("admin").command({ ping: 1 });
             console.log("Mongodb Connected successfully!");
