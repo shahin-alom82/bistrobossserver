dc69f791f361609a976ac8f5f71f83e8625fe007
shahinalom <shahinalom3511371@gmail.com>
2025-03-04 00:07:16 +0600

    token verify admin

--- index.js
+++ index.js
@@ -50,24 +50,48 @@ async function run() {
             const verifyToken = (req, res, next) => {
                   console.log('Insite ferify token', req.headers.authorization)
                   if (!req.headers.authorization) {
-                        return res.status(401).send({ message: 'forbidden access' })
+                        return res.status(401).send({ message: 'unauthorized access' })
                   }
                   const token = req.headers.authorization.split(' ')[1];
                   jwt.verify(token, process.env.ACCESSS_TOKEN_SECRET, (err, decoded) => {
                         if (err) {
-                              return res.status(401).send({ message: 'forbidden access' })
+                              return res.status(401).send({ message: 'unauthorized access' })
                         }
                         req.decoded = decoded
                         next()
                   })
             }
 
+            const verifyAdmin = async (req, res, next) => {
+                  const email = req.decoded.email
+                  const query = { email: email };
+                  const user = await userCollection.findOne(query);
+                  const isAdmin = user?.role === 'admin';
+                  if (!isAdmin) {
+                        return res.status(403).send({ message: 'forbidden asccess' });
+                  }
+                  next()
+            }
+
 
 
             // User Related Data
-            app.get('/users', verifyToken, async (req, res) => {
+            app.get('/users', verifyToken,verifyAdmin, async (req, res) => {
                   const result = await userCollection.find().toArray()
                   res.send(result)
+            })
+            app.get('/users/admin/:email', verifyToken, async (req, res) => {
+                  const email = req.params.email;
+                  if (email !== req.decoded.email) {
+                        return res.status(403).send({ message: 'forbidden access' })
+                  }
+                  const query = { email: email }
+                  const user = await userCollection.findOne(query)
+                  let admin = false
+                  if (user) {
+                        admin = user?.role === 'admin';
+                  }
+                  res.send({ admin })
             })
 
             app.post("/users", async (req, res) => {
