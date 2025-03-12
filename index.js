b432e92bdc8e8cf4840b08046202a097614199bc
shahinalom <shahinalom3511371@gmail.com>
2025-03-12 18:26:11 +0600

    updated

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(
       cors({
             origin: [
                   "http://localhost:5173",
-                  "http://localhost:5000",
+                  "https://bistrobossclient.vercel.app",
             ],
             credentials: true,
       })
